55c2a576d58a59ad5c985a6d768ca1c73cf9aa69
Cristian Bauza <cristian.bauza.silva@gmail.com>
2022-06-02 18:57:53 -0300

    Add project files.

---
+++ DataAccessLayer/DataContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext()
+        {
+        }
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to mysql with connection string from app settings
+            //var connectionString = Configuration.GetConnectionString("WebApiDatabase");
+            //options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            options.UseMySql("server=localhost; Port=1010; database=webapi; user=webapi; password=webapi", 
+                             ServerVersion.AutoDetect("server=localhost; Port=1010; database=webapi; user=webapi; password=webapi"));
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
